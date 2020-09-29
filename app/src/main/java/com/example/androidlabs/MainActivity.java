d4ae08db085c930224656c38d34b1b29eaf5582f
liu00507 <liu00507@algonquinlive.com>
2020-09-29 18:23:32 -0400

    lab2 init commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_grid);
-        final TextView mytxtv=findViewById(R.id.textView);
         Button btn=findViewById(R.id.button2);
+        Switch sw =findViewById(R.id.switch1);
+        sw.setChecked(false);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {mytxtv.setText(R.string.toast_message);}
+            public void onClick(View v) {Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_LONG).show();}
+        });
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
+                Snackbar.setAction( "Undo", click -> sw.setChecked(!isChecked));
+            }
         });
     }
+
 }
