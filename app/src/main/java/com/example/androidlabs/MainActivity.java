d7339a51ea824da4a6a7d30ab6d16013bce7d026
liu00507 <liu00507@algonquinlive.com>
2020-10-02 12:51:37 -0400

    lab2 almost final commit, need to translate strings

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_grid);
+       // LinearLayout LL=findViewById(R.id.linear);
+        final GridLayout GL=findViewById(R.id.grid);
+       // RelativeLayout RL=findViewById(R.id.relative);
         Button btn=findViewById(R.id.button2);
-        Switch sw =findViewById(R.id.switch1);
+        final Switch sw =findViewById(R.id.switch1);
         sw.setChecked(false);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +35,15 @@ public class MainActivity extends AppCompatActivity {
         });
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
-                Snackbar.setAction( "Undo", click -> sw.setChecked(!isChecked));
+            public void onCheckedChanged(CompoundButton cb, final boolean isChecked) {
+                Snackbar sb=Snackbar.make(GL,"The switch is now "+(isChecked? "on":"off"),Snackbar.LENGTH_LONG);
+                sb.setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sw.setChecked(!isChecked);
+                    }
+                });
+                sb.show();
             }
         });
     }
